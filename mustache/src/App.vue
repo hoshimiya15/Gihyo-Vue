f9ff1b341e5c7a08b1e218463171bb62b6b10cd5
hoshimiya15 <nakai.tomohisa@gmail.com>
2025-03-28 23:00:11 +0900

    directive-bind-basic

--- mustache/src/App.vue
+++ mustache/src/App.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 
-const radiusInit = Math.round(Math.random() * 10)
-
-const PI = ref(3.14)
-
-const radius = ref(radiusInit)
-
-const area = computed((): number => {
-  return radius.value * radius.value * PI.value
-})
-
-setInterval((): void => {
-  radius.value = Math.round(Math.random() * 10)
-}, 1000)
+const url = ref('https://vuejs.org/')
 </script>
 
 <template>
-  <p>半径{{ radius }}の円の面積を円周率{{ PI }}で計算すると{{ area }}</p>
+  <p><a v-bind:href="url" target="_blank">Vue.jsのサイト</a></p>
+  <p><a :href="url" target="_blank">Vue.jsのサイト（省略形）</a></p>
+  <p><a v-bind:href="url + 'guide/introduction.html'" target="_blank">Vue.jsガイドのページ</a></p>
 </template>
