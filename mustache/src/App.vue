7998f45dc3e5c04a84232757a2225ed41fadcfa4
hoshimiya15 <nakai.tomohisa@gmail.com>
2025-03-28 23:19:17 +0900

    directive-bind-dynamic

--- mustache/src/App.vue
+++ mustache/src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 
-const url = ref('https://vuejs.org/')
+const widthOrHeight = ref('height')
+const widthOrHeightValue = ref(500)
 </script>
 
 <template>
-  <p><a v-bind:href="url" target="_blank">Vue.jsのサイト</a></p>
-  <p><a :href="url" target="_blank">Vue.jsのサイト（省略形）</a></p>
-  <p><a v-bind:href="url + 'guide/introduction.html'" target="_blank">Vue.jsガイドのページ</a></p>
+  <p><img alt="VueLogo" src="./assets/logo.svg" v-bind:[widthOrHeight]="widthOrHeightValue" /></p>
 </template>
