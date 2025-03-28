4be13fb5e08031d5ece2ab8f4505e8a2b15bc7d6
hoshimiya15 <nakai.tomohisa@gmail.com>
2025-03-28 17:17:35 +0900

    reactive-system(うまく動かない)

--- mustache/src/App.vue
+++ mustache/src/App.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
 import {ref} from "vue"
 
-const name = ref("田中太郎")
+const now = new Date()
+const nowStr = now.toLocaleTimeString()
+
+let timeStr = nowStr
+const timeStrRef = ref(nowStr)
+
+function changeTime(): void {
+  const newTime = new Date()
+  const newTimeStr = newTime.toLocaleTimeString()
+  timeStr = newTimeStr
+  timeStrRef.value = newTimeStr
+}
+
+setInterval(changeTime, 1000)
 </script>
 
 <template>
-  <h1>{{name}}</h1>
+  <h1>現在時刻：{{timeStr}}</h1>
+  <h1>現在時刻(ref)：{{timeStrRef}}</h1>
 </template>
